beed9f5708a0e30ce65834d374f7feda106b091f
bsdillon <57143227+bsdillon@users.noreply.github.com>
2020-01-08 18:33:16 -0500

    Fixed typos

--- Police/car.js
+++ Police/car.js
@@ -27,12 +27,12 @@ function Car(lane)
   this.yVel=Math.floor(Math.random()*20+5);
 }
 
-Car.prototype.draw(color,lane,modulo)
+Car.prototype.draw(modulo)
 {
   ctx.drawImage(cars[this.img],this.x+margin,this.y,carWidth,carLength);
 }
 
-Car.prototype.move(color,lane,modulo)
+Car.prototype.move()
 {
   this.y+=this.yVel;
 }
@@ -58,7 +58,7 @@ Police.prototype.draw(modulo)
   }
 }
 
-Car.prototype.move(color,lane,modulo)
+Car.prototype.move()
 {
   this.x+=this.xVel;
 }
