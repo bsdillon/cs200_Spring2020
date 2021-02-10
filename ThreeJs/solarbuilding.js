626993643ffde0add21eb5dd4c48dbc536f8951d
bsdillon <57143227+bsdillon@users.noreply.github.com>
2021-02-10 00:50:56 -0500

    Added double window walls

--- ThreeJs/solarbuilding.js
+++ ThreeJs/solarbuilding.js
@@ -5,4 +5,17 @@ function buildAll()
   scene.add(drawPanel(farLeft,level,back,farRight,level+.25,front,"foundation"));
   level += .35;
   
+  
+  scene.add(drawPanel(farLeft,level,front,farLeft+10,level+height,front,"dWindow"));
+  scene.add(drawPanel(farLeft+10,level,front,farLeft+20,level+height,front,"dWindow"));
+  scene.add(drawPanel(farLeft+20,level,front,farLeft+30,level+height,front,"dWindow"));
+  scene.add(drawPanel(0,level,front,10,level+height,front,"dWindow"));
+  scene.add(drawPanel(10,level,front,20,level+height,front,"dWindow"));
+  scene.add(drawPanel(20,level,front,30,level+height,front,"dWindow"));
+  scene.add(drawPanel(30,level,front,40,level+height,front,"dWindow"));
+
+  scene.add(drawPanel(farLeft+10,level,back,farLeft+20,level+height,back,"dWindow"));
+  scene.add(drawPanel(farLeft+20,level,back,farLeft+30,level+height,back,"dWindow"));
+  scene.add(drawPanel(10,level,back,20,level+height,back,"dWindow"));
+  scene.add(drawPanel(20,level,back,30,level+height,back,"dWindow"));
 }
