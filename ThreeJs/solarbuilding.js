ff13a296369f30c3776e1a4b99bf0798f859a2ac
bsdillon <57143227+bsdillon@users.noreply.github.com>
2021-02-10 00:20:54 -0500

    Create solarbuilding.js

---
+++ ThreeJs/solarbuilding.js
@@ -0,0 +1,8 @@
+function buildAll()
+{
+  //8ft w x 8.5ft h x 40ft l
+  //concrete floor
+  scene.add(drawPanel(farLeft,level,back-(2*width),farRight,level+.25,front+width,"foundation"));
+  level += .35;
+  
+}
