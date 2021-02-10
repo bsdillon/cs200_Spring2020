9bcf0f5a34d6c67588f9ea9e7dda58380c0bbad8
bsdillon <57143227+bsdillon@users.noreply.github.com>
2021-02-10 00:29:40 -0500

    Added new siding texture

--- ThreeJs/utils.js
+++ ThreeJs/utils.js
@@ -40,6 +40,7 @@ function loadTextures()
   textures["woodLeg"] = makeMaterial("woodleg.png",false,false);
   textures["woodTable"] = makeMaterial("woodtable.png",false,false);
   textures["shingles"] = makeMaterial("shingles.jpg",true,true);
+  textures["siding"] = makeMaterial("siding.png",true,true);
 }
 
 function createBar(x1,y1,z1,x2,y2,z2,textureName)
