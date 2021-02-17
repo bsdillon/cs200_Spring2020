d978177ea97780700c5e8be44e0940d64427ca50
bsdillon <57143227+bsdillon@users.noreply.github.com>
2021-02-16 23:44:24 -0500

    Update solarbuilding.js

--- ThreeJs/solarbuilding.js
+++ ThreeJs/solarbuilding.js
@@ -6,20 +6,26 @@ function buildAll()
   level += .35;
   
   
-  scene.add(drawPanel(farLeft,level,back,farLeft+10,level+height,back,"dWindow"));
-  scene.add(drawPanel(farLeft+10,level,back,farLeft+20,level+height,back,"dWindow"));
-  scene.add(drawPanel(farLeft+20,level,back,farLeft+30,level+height,back,"dWindow"));
-  scene.add(drawPanel(0,level,back,farLeft+30,level+height,back,"rDoor"));
+  scene.add(drawPanel(-40,level,back,-40,level+height,back+10,"sWall"));
+  scene.add(drawPanel(-40,level,back,-30,level+height,back,"dWindow"));
+  scene.add(drawPanel(-30,level,back,-20,level+height,back,"dWindow"));
+  scene.add(drawPanel(-20,level,back,-10,level+height,back,"dWindow"));
+  scene.add(drawPanel(-10,level,back,0,level+height,back,"rDoor"));
   scene.add(drawPanel(0,level,back,10,level+height,back,"dWindow"));
   scene.add(drawPanel(10,level,back,20,level+height,back,"dWindow"));
   scene.add(drawPanel(20,level,back,30,level+height,back,"dWindow"));
   scene.add(drawPanel(30,level,back,40,level+height,back,"dWindow"));
+  scene.add(drawPanel(40,level,back,40,level+height,back+10,"sWall"));
 
   
-  scene.add(drawPanel(farLeft+10,level,front,farLeft+20,level+height,front,"dWindow"));
-  scene.add(drawPanel(farLeft+20,level,front,farLeft+30,level+height,front,"dWindow"));
-  scene.add(drawPanel(farLeft+30,level,front,0,level+height,front,"rDoor"));
+  scene.add(drawPanel(-40,level,front,-40,level+height,front-10,"sWall"));
+  scene.add(drawPanel(-40,level,front,-30,level+height,front,"sWall"));
+  scene.add(drawPanel(-30,level,front,-20,level+height,front,"dWindow"));
+  scene.add(drawPanel(-20,level,front,-10,level+height,front,"dWindow"));
+  scene.add(drawPanel(-10,level,front,0,level+height,front,"rDoor"));
   scene.add(drawPanel(0,level,front,10,level+height,front,"dWindow"));
   scene.add(drawPanel(10,level,front,20,level+height,front,"dWindow"));
   scene.add(drawPanel(20,level,front,30,level+height,front,"dWindow"));
+  scene.add(drawPanel(30,level,front,40,level+height,front,"sWall"));
+  scene.add(drawPanel(40,level,front,40,level+height,front-10,"sWall"));
 }
