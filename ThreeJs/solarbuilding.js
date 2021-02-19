c2195ead9de22970704d2d6556930c5becc44ba2
bsdillon <57143227+bsdillon@users.noreply.github.com>
2021-02-18 19:21:49 -0500

    Update solarbuilding.js

--- ThreeJs/solarbuilding.js
+++ ThreeJs/solarbuilding.js
@@ -71,9 +71,9 @@ function buildAll()
   level+=7.5;
   
   //beam
-  scene.add(drawPanel(27,level,front,33,level+.08,back,"beam"));
-  scene.add(drawPanel(27,level+.92,front,33,level+1,back,"beam"));
-  scene.add(drawPanel(29.75,level,front,30.25,level+1,back,"beam"));
+  scene.add(drawPanel(29.75,level,front,30.25,level+.08,back,"beam"));
+  scene.add(drawPanel(29.75,level+.92,front,30.25,level+1,back,"beam"));
+  scene.add(drawPanel(29.95,level,front,30.05,level+1,back,"beam"));
   
   for(let i=0;i<20-.3;i+=1.5)
   {
