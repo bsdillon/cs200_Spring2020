529a7c13f64eab00872cb59ae2616e939071a709
bsdillon <57143227+bsdillon@users.noreply.github.com>
2021-02-21 16:36:38 -0500

    Update solarbuilding.js

--- ThreeJs/solarbuilding.js
+++ ThreeJs/solarbuilding.js
@@ -2,7 +2,7 @@ function buildAll()
 {
   //8ft w x 8.5ft h x 40ft l
   //concrete floor
-  scene.add(createBar(farLeft,level,back,farRight,level+.25,front,"foundation"));
+  //scene.add(createBar(farLeft,level,back,farRight,level+.25,front,"foundation"));
   level += .35;
   var margin = .1;
  
