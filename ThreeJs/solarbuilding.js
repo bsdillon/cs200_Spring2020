57d7406ef75e48b65347b31103bdab283662af72
bsdillon <57143227+bsdillon@users.noreply.github.com>
2021-02-22 22:41:32 -0500

    Update solarbuilding.js

--- ThreeJs/solarbuilding.js
+++ ThreeJs/solarbuilding.js
@@ -67,12 +67,12 @@ function buildAll()
   scene.add(drawPanel(40+margin,level,front,40+margin,level+height,front-10,"cSolid"));
 
   //truss over kitchen side
-  var tmp = drawPanel(-40-margin,level+height,back,-40-margin,level+height+3,front,"cTruss");
+  var tmp = drawPanel(-40-margin,level+height,back-.25,-40-margin,level+height+3,front+.25,"cTruss");
   tmp.rotateY( Math.PI);
   scene.add(tmp);
   for(let i=0;i<40;i+=10)
   {
-    tmp = drawPanel(i-40,level+height,back,i-40,level+height+3,front,"truss");
+    tmp = drawPanel(i-40,level+height,back-.25,i-40,level+height+3,front+.25,"truss");
     tmp.rotateY( Math.PI);
     scene.add(tmp);
   }
@@ -178,12 +178,12 @@ function buildAll()
   scene.add(drawPanel(20,level+height,back-margin,30,level+height+3,back-margin,"csSolid"));
   scene.add(drawPanel(30,level+height,back-margin,40,level+height+3,back-margin,"csSolid"));
 
-  var tmp = drawPanel(40+margin,level+height,back,40+margin,level+height+3,front,"cTruss");
+  var tmp = drawPanel(40+margin,level+height,back-.25,40+margin,level+height+3,front+.25,"cTruss");
   tmp.rotateY( Math.PI);
   scene.add(tmp);
-  for(let i=10;i<40;i+=10)
+  for(let i=0;i<=40;i+=10)
   {
-    tmp = drawPanel(i,level+height,back,i,level+height+3,front,"truss");
+    tmp = drawPanel(i,level+height,back-.25,i,level+height+3,front+.25,"truss");
     tmp.rotateY( Math.PI);
     scene.add(tmp);
   }
