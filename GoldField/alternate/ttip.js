57fc387297a5418437aa07260357cf0310943f37
bsdillon <57143227+bsdillon@users.noreply.github.com>
2021-10-31 21:46:21 -0400

    Update ttip.js

--- GoldField/alternate/ttip.js
+++ GoldField/alternate/ttip.js
@@ -1,4 +1,26 @@
-function ttip(text)
-{
-  
-}
+      var ttips = {};
+      
+      function addToolTip(id,value)
+      {
+        ttips[id]=value;
+      }
+            
+      function showTTip(ttID)
+      {
+        var tag = document.getElementById("tooltip");
+        tag.innerHTML=ttips[ttID];
+        var loc = document.getElementById(ttID);
+        var rect = loc.getBoundingClientRect();
+        tag.style.left = rect.left + window.scrollX + 5;
+        tag.style.top = rect.top + window.scrollY + 5;
+        tag.style.visible = "visible";
+        tag.style.display = "block"
+      }
+      
+      function hideTTip()
+      {
+        var tag = document.getElementById("tooltip");
+        tag.innerHTML="empty";
+        tag.style.visible = "hidden";
+        tag.style.display = "none"
+      }
