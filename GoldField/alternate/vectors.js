fec5e00f01fabc07c7ca3a077e76a6895cc70440
bsdillon <57143227+bsdillon@users.noreply.github.com>
2021-12-02 18:24:15 -0500

    Update vectors.js

--- GoldField/alternate/vectors.js
+++ GoldField/alternate/vectors.js
@@ -24,7 +24,7 @@
           this.ctx.lineTo(block*(this.xStart+this.xHat), block*(this.yStart+this.yHat));
           var theta =  Math.atan2(this.yStart+this.yHat, this.xStart+this.xHat)+(135*Math.PI/180);
           this.ctx.moveTo(block*(this.xStart+this.xHat), block*(this.yStart+this.yHat));
-          this.ctx.moveTo(block*(this.xStart+Math.cos(theta)), block*(this.yStart-Math.sin(theta));          
+          this.ctx.moveTo(block*(this.xStart+Math.cos(theta)), block*(this.yStart-Math.sin(theta)));          
         this.ctx.drawPath();
         this.ctx.lineWidth = wasWidth;
       }
