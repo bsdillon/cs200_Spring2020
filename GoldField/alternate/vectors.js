6f2dfee6eaa4beebc86f1fb527178967a856b7f9
bsdillon <57143227+bsdillon@users.noreply.github.com>
2021-12-02 19:05:14 -0500

    Update vectors.js

--- GoldField/alternate/vectors.js
+++ GoldField/alternate/vectors.js
@@ -13,24 +13,25 @@
 
       Vector.prototype.DrawLayer = function(block)
       {
+        var halfBlock = block/2;
         this.ctx.fillStyle = this.color;
         this.ctx.strokeStyle = this.color;
         this.ctx.beginPath();
-          this.ctx.arc(block*this.xStart, block*this.yStart, block/4, 0, 2 * Math.PI);
+          this.ctx.arc(block*this.xStart-halfBlock, block*this.yStart-halfBlock, block/4, 0, 2 * Math.PI);
         this.ctx.fill();
         var wasWidth = this.ctx.lineWidth;
         this.ctx.lineWidth = 5;
         this.ctx.beginPath();
-          this.ctx.moveTo(block*this.xStart, block*this.yStart);
-          this.ctx.lineTo(block*(this.xStart+this.xHat), block*(this.yStart+this.yHat));
+          this.ctx.moveTo(block*this.xStart-halfBlock, block*this.yStart-halfBlock);
+          this.ctx.lineTo(block*(this.xStart+this.xHat)-halfBlock, block*(this.yStart+this.yHat)-halfBlock);
         this.ctx.stroke();
         this.ctx.beginPath();
           var theta =  Math.atan2(-this.yHat, this.xHat);
           var theta2= theta+(135*Math.PI/180);
-          this.ctx.moveTo(block*(this.xStart+this.xHat), block*(this.yStart+this.yHat));
-          this.ctx.lineTo(block*(this.xStart+this.xHat+(.5*Math.cos(theta2))), block*(this.yStart+this.yHat-(.5*Math.sin(theta2))));
+          this.ctx.moveTo(block*(this.xStart+this.xHat)-halfBlock, block*(this.yStart+this.yHat)-halfBlock);
+          this.ctx.lineTo(block*(this.xStart+this.xHat+(.5*Math.cos(theta2)))-halfBlock, block*(this.yStart+this.yHat-(.5*Math.sin(theta2)))-halfBlock);
           var theta2= theta-(135*Math.PI/180);
-          this.ctx.lineTo(block*(this.xStart+this.xHat+(.5*Math.cos(theta2))), block*(this.yStart+this.yHat-(.5*Math.sin(theta2))));
+          this.ctx.lineTo(block*(this.xStart+this.xHat+(.5*Math.cos(theta2)))-halfBlock, block*(this.yStart+this.yHat-(.5*Math.sin(theta2)))-halfBlock);
           this.ctx.closePath();
           this.ctx.fill();
           this.ctx.lineWidth = wasWidth;
